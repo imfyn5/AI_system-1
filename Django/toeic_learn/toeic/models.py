d96c380c6c6e5a85208f57f4ac953968924c34ec
ruo <11146077@ntub.edu.tw>
2025-05-23 21:07:48 +0800

    vocab_test update

--- Django/toeic_learn/toeic/models.py
+++ Django/toeic_learn/toeic/models.py
@@ -11,3 +11,21 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class Question(models.Model):
+    QUESTION_TYPE_CHOICES = [
+        ('vocab', 'Vocabulary'),
+        ('reading', 'Reading'),
+        ('listen', 'Listening'),
+    ]
+
+    question_type = models.CharField(max_length=20, choices=QUESTION_TYPE_CHOICES, default='vocab')
+    question_text = models.TextField()
+    option_a_text = models.TextField()
+    option_b_text = models.TextField()
+    option_c_text = models.TextField()
+    option_d_text = models.TextField()
+    is_correct = models.CharField(max_length=1)  # 存 'a'、'b'、'c'、'd'
+    difficulty_level = models.IntegerField(default=1)
+    explanation = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
